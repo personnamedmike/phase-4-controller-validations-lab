e9372d8ff60b01bfa40ce3b391cce913ced2a7ca
Mike Starr <imhs@protonmail.com>
2022-03-16 15:37:43 -0700

    commit tree cf8e1cd2a94c0c7feda853cf3bbab22e8acb96dd

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -10,12 +10,15 @@ class AuthorsController < ApplicationController
     author = Author.create(author_params)
 
     render json: author, status: :created
+
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
   
   def author_params
-    params.permit(:email, :name)
+    params.permit(:email, :name, :title)
   end
   
 end
